b5fafa009abff3628b3cfdd39d13263c72e81c24
LUIS-PC <LUIS-PC@LUIS>
2017-12-01 19:04:09 +0100

    Metodos get de Triangulo

--- src/arrays/Triangulo.java
+++ src/arrays/Triangulo.java
@@ -7,4 +7,26 @@ public class Triangulo {
     private Punto verticeB;
     private Punto verticeC;
     
+    
+    public Triangulo(Punto verticeA, Punto verticeB, Punto verticeC){
+        
+        this.verticeA=verticeA;
+        this.verticeB=verticeB;
+        this.verticeC=verticeC;
+        
+    }
+    
+    public Punto getVerticeA(){
+        return verticeA;
+    }
+    
+    public Punto getVerticeb(){
+        return verticeB;
+    }
+    
+    public Punto getVerticeC(){
+        return verticeC;
+    }
+    
+    
 }
